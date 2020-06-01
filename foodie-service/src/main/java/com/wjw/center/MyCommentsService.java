00bef642ebfcf152391b94277e83f3ddd10b532f
wjw <wjwjava01@163.com>
2020-06-02 00:26:38 +0800

    21 commit:商品评价模块

--- foodie-service/src/main/java/com/wjw/center/MyCommentsService.java
+++ foodie-service/src/main/java/com/wjw/center/MyCommentsService.java
@@ -1,10 +1,39 @@
 package com.wjw.center;
 
+import com.wjw.pojo.OrderItems;
+import com.wjw.pojo.bo.center.OrderItemsCommentBO;
+import com.wjw.utils.PagedGridResult;
+
+import java.util.List;
+
 /**
  * @author : wjwjava01@163.com
  * @date : 23:41 2020/5/25
  * @description : 商品评论
  */
 public interface MyCommentsService {
+    /**
+     * 根据订单id查询关联的商品
+     * @param orderId
+     * @return
+     */
+    List<OrderItems> queryPendingComment(String orderId);
 
+    /**
+     * 保存评论内容
+     * @param orderId
+     * @param userId
+     * @param commentList
+     * @return
+     */
+    void saveComments(String orderId,String userId,List<OrderItemsCommentBO> commentList);
+
+    /**
+     * 查询历史评论内容
+     * @param userId
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    PagedGridResult queryMyComments(String userId, Integer page, Integer pageSize);
 }
